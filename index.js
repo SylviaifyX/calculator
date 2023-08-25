45d6e68a66338bb44edf90361d73882b644d9fbb
SylviaifyX <ifeomasylviaokpara@gmail.com>
2023-08-25 13:05:59 +0100

    yr,mnth,day input change

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
 let activeBtn = document.getElementById("active");
-let birthyear = document.getElementById("year")
-let monthday = document.getElementById("month")
-let Dday = document.getElementById("day")
+let birthyear = document.getElementById("year");
+let monthday = document.getElementById("month");
+let Dday = document.getElementById("day");
+let dayError = document.getElementById("errorDaymessage");
+let monthError = document.getElementById("errorMonthmessage");
+let yearError = document.getElementById("errorYearmessage")
 let message = document.getElementsByClassName("message");
 let Year_result = document.getElementById("years_result");
 let Month_result = document.getElementById("months_result");
@@ -17,13 +20,10 @@ function calculateAge(){
     // console.log("your age is:", result);
     /*  testing code end*/
 
-
-    
     /* getting input values */
     let inputYear = parseInt(birthyear.value);
     let inputMonth = parseInt(monthday.value);
     let inputDday = parseInt(Dday.value);
-
 
     /* getting new Date */
     let new_date = new Date();
@@ -43,18 +43,24 @@ function calculateAge(){
             User_year = User_year - 1;
         }
     }
+
     let  age = User_date - inputDday;
     let  year = User_year - inputYear;
     let month = User_month - inputMonth;
 
-    // if (User_date >= inputDday){
-    //     User_date = User_date - inputDday;
+    if (inputDday > 31){
+        dayError.textContent="must be a valid date"
+        return;
+    }else{
+        dayError.textContent=""
+    }
 
-    // }else{
-    //     User_date--;
-    //     User_date = user_month + User_date - inputDday
-    // }
-
+    if (inputDday === 0){
+        dayError.textContent="value is incomplete"
+        return;
+    }else{
+        dayError.textContent=""
+    }
     // if(User_month >= inputMonth){
     //     User_month = User_month - inputMonth ;
     // }else{
@@ -71,8 +77,7 @@ function calculateAge(){
     //         let User_year = User_year - 1;
     //     }
     // }
-
-    
+      
     Year_result.innerHTML = year;
     Month_result.innerHTML = month;
     Day_result.innerHTML = age  
@@ -88,11 +93,139 @@ function calculateAge(){
    console.log();(RetriveDate);
     
 }
+//validating dayInput Month and Year inputs//
+function dayInput(){
 
+    let inputYear = parseInt(birthyear.value);
+    let inputMonth = parseInt(monthday.value);
+    let inputDday = parseInt(Dday.value);
 
+    /* getting new Date */
+    let new_date = new Date();
 
+    let User_date = new_date.getDate();
+    let User_month = 1 + new_date.getMonth();
+    let User_year = new_date.getFullYear();
+    let user_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    /* calculating the age */
+    if (inputDday > User_date){
+        User_date = User_date + user_month[User_month - 1];
+        User_month = User_month - 1;
+    }else{
+        if (inputMonth > User_month){
+            User_month = User_month + 12;
+            User_year = User_year - 1;
+        }
+    }
+    if (inputDday > 31){
+        dayError.textContent="must be a valid date"
+        return;
+    }else{
+        dayError.textContent=""
+    }
+
+    if (inputDday === 0){
+        dayError.textContent="value is incomplete"
+        return;
+    }else{
+        dayError.textContent=""
+    }
+
+}
+
+function MonthInput(){
+
+    let inputYear = parseInt(birthyear.value);
+    let inputMonth = parseInt(monthday.value);
+    let inputDday = parseInt(Dday.value);
+
+    /* getting new Date */
+    let new_date = new Date();
+
+    let User_date = new_date.getDate();
+    let User_month = 1 + new_date.getMonth();
+    let User_year = new_date.getFullYear();
+    let user_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    /* calculating the age */
+    if (inputDday > User_date){
+        User_date = User_date + user_month[User_month - 1];
+        User_month = User_month - 1;
+    }else{
+        if (inputMonth > User_month){
+            User_month = User_month + 12;
+            User_year = User_year - 1;
+        }
+    }
+    if (inputMonth > 12){
+        monthError.textContent="enter a valid month"
+        return;
+    }else{
+        monthError.textContent=""
+    }
+
+    if (inputDday === 0){
+        monthError.textContent="value is incomplete"
+        return;
+    }else{
+        monthError.textContent=""
+    }
+
+}
+
+function YearInput(){
+
+    let inputYear = parseInt(birthyear.value);
+    let inputMonth = parseInt(monthday.value);
+    let inputDday = parseInt(Dday.value);
+
+    /* getting new Date */
+    let new_date = new Date();
+
+    let User_date = new_date.getDate();
+    let User_month = 1 + new_date.getMonth();
+    let User_year = new_date.getFullYear();
+    let user_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    /* calculating the age */
+    if (inputDday > User_date){
+        User_date = User_date + user_month[User_month - 1];
+        User_month = User_month - 1;
+    }else{
+        if (inputMonth > User_month){
+            User_month = User_month + 12;
+            User_year = User_year - 1;
+        }
+    }
+    if (inputYear > 2023){
+        yearError.textContent="Not yet born"
+        return;
+    }else{
+        yearError.textContent=""
+    }
+
+    if (inputDday === 0){
+        yearError.textContent="value is incomplete"
+        return;
+    }else{
+        monthError.textContent=""
+    }
+
+}
 
 activeBtn.addEventListener("click", function (){
     calculateAge()
-   
-})
+});
+
+monthday.addEventListener("input", function (){
+    MonthInput()
+});
+
+Dday.addEventListener("input", function (){
+    dayInput()
+});
+
+birthyear.addEventListener("input", function (){
+    YearInput()
+});
